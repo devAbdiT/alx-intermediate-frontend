6761dd79cad91fc4cee7484d27e8f01f1642ad79
Abdi begna <abditadesse89@gmail.com>
2025-11-03 20:49:18 +0300

    Index.html + Tailwind

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    // Add ALL paths where you use Tailwind classes
+    "./*.html",
+    "./src/*.html",
+    "./**/*.html",
+    // Or be more specific:
+    // "./index.html",
+    // "./**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
